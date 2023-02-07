1a5f1281c58b3717fdb0564624108f621563a21e
Isaac Lavender <musicman241504@gmail.com>
2023-02-07 21:05:38 +0000

    8 threads

--- nodeCalc.py
+++ nodeCalc.py
@@ -4,6 +4,7 @@ import time
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
 
 
 #Each node will preform a differnt thing to a number
@@ -14,11 +15,11 @@ if rank == 0:
     num = 1
     
     while True:
-        node = rand.randint(1,4)
+        node = rand.randint(1,size-1)
         comm.send(num,dest=node)
         num = comm.recv(source=node)
         print(num)
-        time.sleep(1)
+        #time.sleep(1)
 
 
 
@@ -37,11 +38,29 @@ if rank == 2:
 if rank == 3:
     while True:
         num = comm.recv(source=0)
-        num = num*3
+        num = num%100
         comm.send(num,dest=0)
 
 if rank == 4:
     while True:
         num = comm.recv(source=0)
         num = num%10
+        comm.send(num,dest=0)
+
+if rank == 5:
+    while True:
+        num = comm.recv(source=0)
+        num = num%50
+        comm.send(num,dest=0)
+
+if rank == 6:
+    while True:
+        num = comm.recv(source=0)
+        num = num**2
+        comm.send(num,dest=0)
+
+if rank == 7:
+    while True:
+        num = comm.recv(source=0)
+        num = num*-1
         comm.send(num,dest=0)
