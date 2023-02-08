f4872eaf1d0b48dab60cd48320187032e8b356e1
Isaac Lavender <musicman241504@gmail.com>
2023-02-08 18:15:51 +0000

    ping

---
+++ ping.py
@@ -0,0 +1,27 @@
+from mpi4py import MPI
+import time
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
+#Calculate ping time for each node
+
+
+if rank == 0:
+    num = 1
+    for i in range(1,size):
+        start = time.time_ns()
+        for j in range(100000):
+            comm.send(num,dest=i)
+            comm.recv(source=i)
+        end = time.time_ns()
+        time_ns = (end-start)/100000
+        print("Rank " + str(i) + ": " + str(round((time_ns)/1000,3)) + " μs")
+
+
+else:
+    while True:
+        num = comm.recv(source=0)
+        comm.send(num,dest=0)
