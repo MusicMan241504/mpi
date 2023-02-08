3287df4841f2141b6570c92b3774f0e4a453c155
Isaac Lavender <musicman241504@gmail.com>
2023-02-08 19:56:17 +0000

    numpy

--- ping_np.py
+++ ping_np.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import time
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -10,12 +11,12 @@ size = comm.Get_size()
 
 
 if rank == 0:
-    num = [i for i in range(100)]
+    data = np.arange(10000,dtype='i')
     for i in range(1,size):
         start = time.time_ns()
         for j in range(100000):
-            comm.send(num,dest=i)
-            comm.recv(source=i)
+            comm.Send(data,dest=i)
+            comm.Recv(data,source=i)
         end = time.time_ns()
         time_ns = (end-start)/100000
         print("Rank " + str(i) + ": " + str(round((time_ns)/1000,3)) + " μs")
@@ -23,5 +24,6 @@ if rank == 0:
 
 else:
     while True:
-        num = comm.recv(source=0)
-        comm.send(num,dest=0)
+        data = np.empty(10000,dtype='i')
+        comm.Recv(data,source=0)
+        comm.Send(data,dest=0)
