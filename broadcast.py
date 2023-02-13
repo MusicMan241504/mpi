14023e1e6b84b3a52845a0c960c33d62b96e2647
Isaac Lavender <musicman241504@gmail.com>
2023-02-13 16:17:56 +0000

    broadcast numpy arrays

--- broadcast.py
+++ broadcast.py
@@ -1,14 +1,13 @@
 from mpi4py import MPI
-import random as rand
-import time
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 if rank == 0:
-    data = [rand.randint(1,100) for i in range(100)]
+    data = np.arange(100,dtype='i')
 else:
-    data = None
-data = comm.bcast(data, root=0)
+    data = np.empty(100,dtype='i')
+comm.Bcast(data, root=0)
 print(data)
 
